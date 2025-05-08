70f240ac66b5c323579cc7ca5cc283cef825e0ee
Frederick Lee <frederick.v.l@gmail.com>
2025-05-08 16:36:01 -0400

    Fix concurrency bug

--- src/solver/det_solver.py
+++ src/solver/det_solver.py
@@ -68,6 +68,8 @@ class DetSolver(BaseSolver):
                 self.train_dataloader.sampler.set_epoch(epoch)
 
             if epoch == self.train_dataloader.collate_fn.stop_epoch:
+                if dist_utils.is_dist_available_and_initialized():
+                    torch.distributed.barrier()
                 self.load_resume_state(str(self.output_dir / "best_stg1.pth"))
                 if self.ema:
                     self.ema.decay = self.train_dataloader.collate_fn.ema_restart_decay
